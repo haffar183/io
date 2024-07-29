36b5ab2ed326d1ce8c8971c0bd69e48a03c903be
muezurrehman <mrehman@allsorter.com>
2024-07-29 23:46:11 +0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'io-app';
+  username : string = '';
+  password : string = '';
+
+  constructor(private http: HttpClient ){
+
+  }
+
+  signIn() {
+    console.log('Username:', this.username);
+    console.log('Password:', this.password);
+    this.calltobackend(this.username, this.password)
+      .subscribe(
+        response => {
+          alert('verified');
+        },
+        error => {
+          alert('not verified');
+        }
+      );
+  }
+
+  calltobackend(username: string, password: string): Observable<any> {
+    const body = { username, password };
+    return this.http.post<any>(`http://127.0.0.1:8080/api/users/signup`, body);
+  }
 }
